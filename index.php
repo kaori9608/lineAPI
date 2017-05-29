5d95b491074a8f4bc457538836f4a63cf9fac76b
藁科かおり <warashinakaori@warashinakaorinoMacBook-Pro.local>
2017-05-29 14:52:55 +0900

    file change

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ this is line api content
 <?php 
 // データベースに接続するために必要なデータソースを変数に格納
   // mysql:host=ホスト名;dbname=データベース名;charset=文字エンコード
-$dsn = 'mysql:us-cdbr-iron-east-03.cleardb.net;dbname=heroku_e84ff0594615ec5;charset=utf8';
+$dsn = 'mysql://b230e075a82da6:36098907@us-cdbr-iron-east-03.cleardb.net/heroku_e84ff0594615ec5?reconnect=true';
   // データベースのユーザー名
 $user = 'b230e075a82da6';
   // データベースのパスワード
