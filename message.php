2fab9cb8dfd4755673195902e88eb95f98dbd96d
藁科かおり <warashinakaori@warashinakaorinoMacBook-Pro.local>
2017-05-29 13:42:36 +0900

    file change

--- message.php
+++ message.php
@@ -121,19 +121,19 @@ if ($message->{"text"} == '出勤') {
          'text' => $message->{"text"}
      ];
 
-     $link = mysql_connect("host=us-cdbr-iron-east-03.cleardb.net", "b230e075a82da6", "36098907", "heroku_e84ff0594615ec5");
-if (!$link) {
-die('接続失敗です。'.mysql_error());
-}
-$db_selected = mysql_select_db('heroku_e84ff0594615ec5', $link);
-if (!$db_selected) {
-die('データベース選択失敗です。'.mysql_error());
-}
-mysql_set_charset('utf8');
-$result = mysql_query("INSERT INTO `test` (`testcol`, `testcol1`) VALUES ($displayName, $messageData)");
-if (!$result) {
-}
-$row = mysql_fetch_assoc($result)
+    $link = mysql_connect("us-cdbr-iron-east-03.cleardb.net", "b230e075a82da6", "36098907", "heroku_e84ff0594615ec5");
+    if (!$link) {
+
+    $db_selected = mysql_select_db('heroku_e84ff0594615ec5', $link);
+    if (!$db_selected) {
+    die('データベース選択失敗です。'.mysql_error());
+    }
+    mysql_set_charset('utf8');
+    $result = mysql_query("INSERT INTO `test` (`testcol`, `testcol1`) VALUES ($displayName, $messageData)");
+    if (!$result) {
+    }
+    $row = mysql_fetch_assoc($result)die('接続失敗です。'.mysql_error());
+    }
 
 
 
