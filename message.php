13baefb2f37fabe7208cd622b7f36510a495b10e
藁科かおり <warashinakaori@warashinakaorinoMacBook-Pro.local>
2017-05-30 13:52:58 +0900

    file change

--- message.php
+++ message.php
@@ -1,27 +1,14 @@
 <?php
 require_once('./vendor/autoload.php');
-require_once('setting.php');
-
+$accessToken = 'aWauEUHKwacwEryzppEW/hoquAKyuCXKQvSbsLggJ7jjG2N/KQTEpvXzoNsm5mESnUOyZfhXn+5WqSIQ5ZggTlvASS0Cy6xvWS1JDXkeDeON3x6pEzaBmKHmljyXyAqOhV8cCJonYvFtHwmXB9AiLAdB04t89/1O/w1cDnyilFU=';
 $jsonString = file_get_contents('php://input');
 error_log($jsonString);
 $jsonObj = json_decode($jsonString);
-
 $message = $jsonObj->{"events"}[0]->{"message"};
 $source = $jsonObj->{"events"}[0]->{"source"};
 $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
-
-
-// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('$ACCESSTOKEN');
-// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '$CHANNEL_SECRET']);
-// $response = $bot->getProfile('$MID');
-// if ($response->isSucceeded()) {
-//     $profile = $response->getJSONDecodedBody();
-//     echo $profile['displayName'];
-//     // echo $profile['pictureUrl'];
-//     // echo $profile['statusMessage'];
-// };
-
-// 送られてきたメッセージの中身からレスポンスのタイプを選択、ボタンの内容を発言にする
+// 送られてきたメッセージの中身からレスポンスのタイプを選択
+// ボタンの内容を発言にする
 if ($message->{"text"} == '出勤') {
     // 確認ダイアログタイプ
     $messageData = [
@@ -132,14 +119,11 @@ if ($message->{"text"} == '出勤') {
     ];
 } else {
     $send_userId = $source->{"userId"}
-    // テキストについての対応
     // それ以外は送られてきたテキストをオウム返し
     $messageData = [
          'type' => 'text',
          'text' => $message->{"text"}
      ];
-
-     //勤務形態などが文章に入っていた場合について
      //勤務の文字置換
     $str = $message->{"text"};
     $search = array('通常出勤','直行','遅出','遅刻','退社','直帰','早退','宿着','明日','明後日','明々後日','今日');
@@ -147,13 +131,29 @@ if ($message->{"text"} == '出勤') {
     $work_datanum = str_replace($search,$replace,$str);
     //時刻の取得
     $now = date('Y-m-d H:i:s');
-
+    function get_profile(){
+        $response = $this->bot->getProfile($this->userId);
+            if ($response->isSucceeded()) {
+            $profile = $response->getJSONDecodedBody();
+            $displayName = $profile['displayName'];
+            $userId = $profile['userId'];
+            $profile_array = array("displayName"=>$displayName,"userId"=>$userId);
+            }
+    }
+    // データベースに接続するために必要なデータソースを変数に格納
+    $dsn = 'mysql:host=us-cdbr-iron-east-03.cleardb.net;dbname=heroku_e84ff0594615ec5;charset=utf8;reconnect=true';
+      // データベースのユーザー名
+    $user = 'b230e075a82da6';
+      // データベースのパスワード
+    $password = '36098907';
+     
     // tryにPDOの処理を記述
     try {
       // PDOインスタンスを生成
-      $dbh = new PDO($DSN, $USER, $PASSWORD);
-    // エラー処理を記述
+      $dbh = new PDO($dsn, $user, $password);
+    // エラー（例外）が発生した時の処理を記述
     } catch (PDOException $e) {
+      // エラーメッセージを表示させる
       echo 'データベースにアクセスできません！' . $e->getMessage();
       exit;
     }
@@ -162,19 +162,16 @@ if ($message->{"text"} == '出勤') {
     // 挿入する値は空のまま、SQL実行の準備をする
     $stmt = $dbh->prepare($sql);
     // 挿入する値を配列に格納する
-    $params = array(':idn_work_time' => '', ':work_date' => $now, ':me_staff_detail_id' => $profile_array->$send_userId, ':me_staff_detail_name' => $profile_array->{"displayName"}, ':wo_work_status_id' => $work_datanum, ':wo_work_status_name' => $message->{"text"}, ':updated' => $now);
+    $params = array(':idn_work_time' => '', ':work_date' => $now, ':me_staff_detail_id' => $send_userId, ':me_staff_detail_name' => $profile_array->{"displayName"}, ':wo_work_status_id' => $work_datanum, ':wo_work_status_name' => $message->{"text"}, ':updated' => $now);
+     
     // 挿入する値が入った変数をexecuteにセットしてSQLを実行
     $stmt->execute($params);
 }
-
 $response = [
     'replyToken' => $replyToken,
     'messages' => [$messageData]
 ];
-
-
 error_log(json_encode($response));
-
 $ch = curl_init('https://api.line.me/v2/bot/message/reply');
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
@@ -182,9 +179,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Content-Type: application/json; charser=UTF-8',
-    'Authorization: Bearer ' . $ACCESSTOKEN
+    'Authorization: Bearer ' . $accessToken
 ));
 $result = curl_exec($ch);
 error_log($result);
 curl_close($ch);
-
