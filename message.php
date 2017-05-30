ee708e64396b3501725695933f043d344f4646f0
藁科かおり <warashinakaori@warashinakaorinoMacBook-Pro.local>
2017-05-30 17:38:31 +0900

    file change

--- message.php
+++ message.php
@@ -12,7 +12,7 @@ $message = $jsonObj->{"events"}[0]->{"message"};
 $replyToken = $jsonObj->{"events"}[0]->{"replyToken"};
 
 // // ユーザ情報
-$send_userId = $jsonObj->{"events"}[0]->{"source"}[0]->{"userId"};
+$send_userId = $jsonObj->{"events"}[0]->{"replyToken"}[0]->{"source"}[0]->{"userId"};
 
 // // sourceからユーザ情報を取得   
 // $send_userId = $source->{"userId"};
